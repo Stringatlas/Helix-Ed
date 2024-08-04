669ac3df6ae177a6d0f631f1b60cc0bc0179659e
Kevin <kevinzhang882019@gmail.com>
2024-08-04 14:42:08 -0700

    Add course cards in enroll page

--- src/lib/components/CourseCard.svelte
+++ src/lib/components/CourseCard.svelte
@@ -6,15 +6,44 @@
 
 <div class="card">
     <h1>{courseData.title}</h1>
+    <div class="status">
+        <div id="indicator" style="background-color: {courseData.status == 'closed' ? 'red' : 'green'};"></div>
+        <p>Registration {courseData.status}</p>
+    </div>
+
     <p>{courseData.description}</p>
 </div>
 
 <style lang="scss">
+    .status {
+        background-color: white;
+        border-radius: 8px;
+        padding: 4px 12px;
+        display: inline-block;
+
+        #indicator {
+            width: 16px;
+            height: 16px;
+            border-radius: 50%;
+            background-color: $primary;
+            display: inline-block;
+            margin-right: 8px;
+        }
+
+        p {
+            display: inline-block;
+        }
+    }
+
     .card {
-        width: 100%;
         height: 100%;
         background-color: $background-color;
         border-radius: 10px;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        box-shadow: 0 8px 12px rgba(0, 0, 0, 0.2);
+        padding: 24px;
+
+        h1 {
+            margin-bottom: 0.6em;
+        }
     }
 </style>
--- src/routes/enroll/+page.svelte
+++ src/routes/enroll/+page.svelte
@@ -8,14 +8,27 @@
 
     $: {
         Object.entries(courseData).forEach(([subject, courses]) => {
-            console.log(subject);
-            console.log(courses);
-            openClasses = openClasses.concat(courses.filter((course) => course.status === "Open"));
-            closedClasses = closedClasses.concat(courses.filter((course) => course.status === "Closed"));
+            openClasses = openClasses.concat(courses.filter((course) => course.status == "open"));
+            closedClasses = closedClasses.concat(courses.filter((course) => course.status == "closed"));
         });
 
         console.log(openClasses);
         console.log(closedClasses);
+
+        // for (const subject in courseData) {
+        //     //@ts-ignore
+        //     for (const course of courseData[subject]) {
+        //         console.log(course);
+        //         if (course.status == "open") {
+        //             openClasses.push(course);
+        //         } else {
+        //             closedClasses.push(course);
+        //         }
+        //     }
+        // }
+
+        // console.log(openClasses);
+        // console.log(closedClasses);
     }
 </script>
 
@@ -24,17 +37,28 @@
         <h1>Our courses</h1>
     </section>
 
-    <main>
+    <div>
         {#each openClasses as course}
             <CourseCard courseData={course} />
         {/each}
         {#each closedClasses as course}
             <CourseCard courseData={course} />
         {/each}
-    </main>
+    </div>
 </main>
 
 <style lang="scss">
+    main {
+        padding: 28px;
+
+        div {
+            display: flex;
+            flex-direction: column;
+            gap: 16px;
+            flex-wrap: nowrap;
+        }
+    }
+
     #header {
         padding: 52px 32px;
 
