100e99b3acde07e73dc80b191ac44e78e348c741
Kevin <kevinzhang882019@gmail.com>
2024-08-08 02:02:15 -0700

    Update hook

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,9 +4,9 @@ export const handle: Handle = async ({ event, resolve }) => {
     const host = event.request.headers.get('host');
     const url = new URL(event.request.url);
 
-    if (host && host.startsWith('biobrawl.') && url.pathname === '/') {
-        url.pathname = '/bio-brawl';
-        return Response.redirect(url.toString(), 301);
+    if (host && host.startsWith('biobrawl.')) {
+        url.pathname = `/bio-brawl${url.pathname}`;
+        return resolve({ ...event, url });
     }
 
     return resolve(event);
